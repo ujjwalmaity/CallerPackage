65de12723a001229b57ebcc01c620707d31cac1d
ujjwal <ujjwalmaity98@gmail.com>
2022-03-19 07:08:59 +0530

    Change callback function in Flutter

--- lib/src/caller_event.dart
+++ lib/src/caller_event.dart
@@ -1,6 +1,19 @@
-enum CallerEvent {
+enum CallAction {
   callEnded,
   onMissedCall,
   onIncomingCallAnswered,
   onIncomingCallReceived,
+}
+
+abstract class BaseCallEvent {}
+
+class CallEvent extends BaseCallEvent {
+  final CallAction action;
+  final String number;
+  final int? duration;
+
+  CallEvent({required this.action, required this.number, this.duration});
+
+  @override
+  String toString() => 'CallEvent { number: $number, duration: $duration }';
 }
