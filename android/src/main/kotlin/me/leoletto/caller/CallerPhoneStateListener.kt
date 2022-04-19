b252db9ede2725dd5d2dacaf88a3783b4e257800
ujjwal <ujjwalmaity98@gmail.com>
2022-04-19 17:55:18 +0530

    Handle exception

--- android/src/main/kotlin/me/leoletto/caller/CallerPhoneStateListener.kt
+++ android/src/main/kotlin/me/leoletto/caller/CallerPhoneStateListener.kt
@@ -6,6 +6,7 @@ import android.telephony.TelephonyManager
 import android.util.Log
 import androidx.annotation.RequiresApi
 import me.leoletto.caller.CallerPlugin.Companion.eventHandler
+import java.lang.Exception
 import java.time.Duration
 import java.time.ZonedDateTime
 import java.util.ArrayList
@@ -17,49 +18,53 @@ class CallerPhoneStateListener internal constructor() : PhoneStateListener() {
     @RequiresApi(Build.VERSION_CODES.O)
     @Synchronized
     override fun onCallStateChanged(state: Int, incomingNumber: String) {
-        // Prepare the arguments to be sent to the callback
-        val arguments = ArrayList<Any?>()
-        arguments.add(incomingNumber)
+        try {
+            // Prepare the arguments to be sent to the callback
+            val arguments = ArrayList<Any?>()
+            arguments.add(incomingNumber)
 
-        when (state) {
-            TelephonyManager.CALL_STATE_IDLE -> {
-                if (previousState == TelephonyManager.CALL_STATE_OFFHOOK) {
-                    val now = ZonedDateTime.now()
-                    val duration = Duration.between(time, now)
-                    Log.d(CallerPlugin.PLUGIN_NAME, "Phone State event CALL_ENDED with number $incomingNumber")
-                    arguments.add("callEnded")
-                    arguments.add(duration.toMillis() / 1000)
-                    time = null
-                    eventHandler.send("incomingCallEnded", arguments)
-                } else if (previousState == TelephonyManager.CALL_STATE_RINGING) {
-                    Log.d(CallerPlugin.PLUGIN_NAME, "Phone State event INCOMING_CALL_MISSED $incomingNumber")
-                    arguments.add("onMissedCall")
-                    arguments.add(null)
-                    eventHandler.send("onMissedCall", arguments)
+            when (state) {
+                TelephonyManager.CALL_STATE_IDLE -> {
+                    if (previousState == TelephonyManager.CALL_STATE_OFFHOOK) {
+                        val now = ZonedDateTime.now()
+                        val duration = Duration.between(time, now)
+                        Log.d(CallerPlugin.PLUGIN_NAME, "Phone State event CALL_ENDED with number $incomingNumber")
+                        arguments.add("callEnded")
+                        arguments.add(duration.toMillis() / 1000)
+                        time = null
+                        eventHandler.send("incomingCallEnded", arguments)
+                    } else if (previousState == TelephonyManager.CALL_STATE_RINGING) {
+                        Log.d(CallerPlugin.PLUGIN_NAME, "Phone State event INCOMING_CALL_MISSED $incomingNumber")
+                        arguments.add("onMissedCall")
+                        arguments.add(null)
+                        eventHandler.send("onMissedCall", arguments)
+                    }
+                    previousState = TelephonyManager.CALL_STATE_IDLE
                 }
-                previousState = TelephonyManager.CALL_STATE_IDLE
-            }
-            TelephonyManager.CALL_STATE_OFFHOOK -> {
-                if (previousState == TelephonyManager.CALL_STATE_RINGING) {
-                    Log.d(CallerPlugin.PLUGIN_NAME, "Phone State event INCOMING_CALL_ANSWERED $incomingNumber")
-                    arguments.add("onIncomingCallAnswered")
-                    arguments.add(null)
-                    eventHandler.send("onIncomingCallAnswered", arguments)
-                }
-                previousState = TelephonyManager.CALL_STATE_OFFHOOK
+                TelephonyManager.CALL_STATE_OFFHOOK -> {
+                    if (previousState == TelephonyManager.CALL_STATE_RINGING) {
+                        Log.d(CallerPlugin.PLUGIN_NAME, "Phone State event INCOMING_CALL_ANSWERED $incomingNumber")
+                        arguments.add("onIncomingCallAnswered")
+                        arguments.add(null)
+                        eventHandler.send("onIncomingCallAnswered", arguments)
+                    }
+                    previousState = TelephonyManager.CALL_STATE_OFFHOOK
 
-                // Get the current time to count later the duration of the call
-                time = ZonedDateTime.now()
-            }
-            TelephonyManager.CALL_STATE_RINGING -> {
-                if (previousState == TelephonyManager.CALL_STATE_IDLE) {
-                    Log.d(CallerPlugin.PLUGIN_NAME, "Phone State event INCOMING_CALL_RECEIVED $incomingNumber")
-                    arguments.add("onIncomingCallReceived")
-                    arguments.add(null)
-                    eventHandler.send("onIncomingCallReceived", arguments)
+                    // Get the current time to count later the duration of the call
+                    time = ZonedDateTime.now()
                 }
-                previousState = TelephonyManager.CALL_STATE_RINGING
+                TelephonyManager.CALL_STATE_RINGING -> {
+                    if (previousState == TelephonyManager.CALL_STATE_IDLE) {
+                        Log.d(CallerPlugin.PLUGIN_NAME, "Phone State event INCOMING_CALL_RECEIVED $incomingNumber")
+                        arguments.add("onIncomingCallReceived")
+                        arguments.add(null)
+                        eventHandler.send("onIncomingCallReceived", arguments)
+                    }
+                    previousState = TelephonyManager.CALL_STATE_RINGING
+                }
             }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
